eeea32004f9cb4d9a877beed5832446a78277415
UmaShankar Sharma <umageorgian3541@gmail.com>
2025-03-30 15:56:30 +0530

    isvalidAnagram

---
+++ src/main/java/arrayAndHashing/twoSum.java
@@ -0,0 +1,30 @@
+package arrayAndHashing;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+
+	
+
+	public static int[] solution1(int[] arr , int target) {
+		
+		Map<Integer , Integer> hm = new HashMap<>();
+		
+
+		for(int i=0 ; i< arr.length ; i++) {
+			int diff = target - arr[i];
+			
+			if(hm.containsKey(diff))
+				return new int[] {i,hm.get(diff)};
+			
+			
+			hm.put(arr[i], i);
+				
+		}
+		
+		return new int[] {0,0};
+		
+	}
+}
