07dd6e0708641343c083283e823ad206cfdbd32c
UmaShankar Sharma <umageorgian3541@gmail.com>
2025-04-04 19:31:12 +0530

    4 april

--- src/main/java/twoPointers/ValidPalindrome.java
+++ src/main/java/twoPointers/ValidPalindrome.java
@@ -10,7 +10,7 @@ public class ValidPalindrome {
 		
 		String s1="A man, a plan, a canal: Panama";
 		
-		System.out.println(isPalindrome(s1));	
+		System.out.println(isPalindromeV2(s1));	
 		
 		
 	}
@@ -44,7 +44,60 @@ public class ValidPalindrome {
 	
 	}
 	
-	
+	public static boolean isPalindromeV2(String s1) {	
+		
+		
+		if(s1.isEmpty())
+			return false;
+        
+        s1 = s1.toLowerCase();
+        
+        int i =0 ;
+        int j = s1.length()-1;	
+        
+		while(i < j){
+			
+			System.out.println("i : "+s1.charAt(i)+" j : "+s1.charAt(j));		
+			
+			while(!Character.isLetterOrDigit(s1.charAt(i))) {
+				
+				
+				System.out.println("Skipped i"+s1.charAt(i));	
+				i++;
+				
+			}
+				
+            
+            while(!Character.isLetterOrDigit(s1.charAt(j))) {
+            	
+            	
+            	System.out.println("Skipped j "+s1.charAt(j));
+            	j--;
+            }
+            
+            
+            
+            
+			
+			if(s1.charAt(i) != s1.charAt(j)) {
+				
+				System.out.println("Not Palindrome i=" + s1.charAt(i) +" j="+ s1.charAt(j)  );	
+				return false;
+				
+				
+			}
+				
+			
+			i++;
+			j--;
+			
+			
+		}
+		
+		
+		
+		return true;
+	}
 	
 	
 }
